6a1a360d478c89459b4baf77d8b805437170b929
Michivi <webmaster@michivi.com>
2020-09-20 11:47:23 +0200

    feat: init code repository

---
+++ README.md
@@ -0,0 +1,9 @@
+# VBF File system
+
+Various tools for VBF files handling, as found in the Final Fantasy X HD Remaster on PC.
+
+**NOTE**: this project is mainly a personal experimentation.
+
+## Development
+
+Use the command `nix-shell` for a dedicated development shell. Using the `stack` command directly is another option.
---
+++ shell.nix
@@ -0,0 +1,50 @@
+{
+  haskellNix ? import (builtins.fetchTarball "https://github.com/input-output-hk/haskell.nix/archive/ca207f93ad2c89e21c458b17084c747093e5be3a.tar.gz") {},
+  pkgs ? import haskellNix.sources.nixpkgs-2003 haskellNix.nixpkgsArgs
+}:
+
+let
+  hsPkgs = import ./default.nix { inherit pkgs; };
+
+  ghcVersion = "8.8.4";
+
+  targetPrefix = with pkgs.stdenv; lib.optionalString (targetPlatform != buildPlatform)
+                                                      (targetPlatform.config + "-");
+
+in
+  hsPkgs.shellFor {
+    packages = ps: with ps; [
+      vbf-fs
+    ];
+
+    withHoogle = true;
+
+    nativeBuildInputs =
+        with pkgs; [ 
+            # Various basic programs.
+            buildPackages.git
+
+            # Haskell tools.
+            buildPackages.cabal-install
+            buildPackages.haskellPackages.brittany
+            buildPackages.haskellPackages.hpack
+            buildPackages.haskellPackages.hspec-discover
+
+            # Linters and checkers.
+            buildPackages.haskellPackages.hlint
+        ];
+    
+    shellHook = ''
+      VBFFS_APP=dist-newstyle/build/x86_64-linux/ghc-8.8.4/vbf-fs-0.1.0.0/x/vbf/build/vbf/vbf
+
+      alias "b"="cabal new-build"
+      alias "c"="hpack && cabal new-configure"
+      alias "d"="cabal haddock"
+      alias "f"="(find app -name '*.hs' ; find src -name '*.hs') | xargs -n1 brittany --write-mode=inplace"
+      alias "l"="hlint -h .hlint.yaml ."
+      alias "lr"="hlint -h .hlint.yaml --report ."
+      alias "r"="$VBFFS_APP"
+    '';
+
+    exactDeps = true;
+  }
