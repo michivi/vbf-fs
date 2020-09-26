7acc25fa9a2b7e5e341343a4713896f93473ce9f
Michivi <webmaster@michivi.com>
2020-09-26 23:57:38 +0200

    feat: add release script for Docker

--- release.sh
+++ release.sh
@@ -2,12 +2,25 @@
 
 set -e
 
-OUTPUT_DIR=$PWD/build
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 
-rm -rf $OUTPUT_DIR 2> /dev/null
+[ -f /etc/os-release ] && . /etc/os-release
+
+OS_ID=${OS_ID:-".${ID}-${VERSION_ID}"}
+SUFFIX="$([ ${#OS_ID} -gt 2 ] && echo -n "$OS_ID" || echo -n "")"
+
+echo "Releasing project from '$SCRIPT_DIR'..."
+
+OUTPUT_DIR=$SCRIPT_DIR/build
+
+[ -d "$OUTPUT_DIR" ] && rm -rf $OUTPUT_DIR/* 2> /dev/null
 mkdir -p $OUTPUT_DIR
 
-stack build --haddock --haddock-arguments "--odir=$OUTPUT_DIR/doc" --copy-bins --local-bin-path=$OUTPUT_DIR/bin
-stack sdist --tar-dir $OUTPUT_DIR/sdist
+stack $VBFFS_STACK_ARGS clean
+stack $VBFFS_STACK_ARGS build --haddock --haddock-arguments "--odir=$OUTPUT_DIR/doc" --copy-bins --local-bin-path=$OUTPUT_DIR/bin/$MACHTYPE$SUFFIX
+tar zcf $OUTPUT_DIR/bin-$MACHTYPE$SUFFIX.tar.gz -C $OUTPUT_DIR bin
+tar zcf $OUTPUT_DIR/haddock.tar.gz -C $OUTPUT_DIR doc
+
+stack $VBFFS_STACK_ARGS sdist --tar-dir $OUTPUT_DIR
 
 echo "Released in $OUTPUT_DIR."
